55d9a02dc1fbe0d6c90f774ce722cab4ae17d0cd
murdermox <max_2196@hotmail.com>
2015-05-22 10:21:06 +0200

    First commit

---
+++ ProjectBlokD/src/projectblokd/ProjectBlokD.java
@@ -0,0 +1,11 @@
+package projectblokd;
+
+
+public class ProjectBlokD {
+
+    public static void main(String[] args) {
+        Level level = new Level();
+        level.makeLevel1();
+    }
+    
+}
